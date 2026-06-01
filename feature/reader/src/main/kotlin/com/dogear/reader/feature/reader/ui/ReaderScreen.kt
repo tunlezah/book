9741aaa5af696ebcfe85572ca4217b1d9ba8408a
Earmark <mpworkaddress@gmail.com>
2026-06-01 10:10:29 +0000

    Fix: import Modifier.focusable from androidx.compose.foundation

--- feature/reader/src/main/kotlin/com/dogear/reader/feature/reader/ui/ReaderScreen.kt
+++ feature/reader/src/main/kotlin/com/dogear/reader/feature/reader/ui/ReaderScreen.kt
@@ -42,11 +42,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.foundation.focusable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.focus.focusable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyEvent
